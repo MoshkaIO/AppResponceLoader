8c56b06087fd1c4413d859c8e52d23d352170fda
Mihail Ioffe <19657132f@gmail.com>
2026-02-03 17:44:30 +0300

    Добавил комментарии ко всем функциям (кроме тестовых) 	modified:   module_converter.py 	modified:   module_logger_tech.py 	modified:   module_read_text.py 	modified:   module_write_text.py

--- module_converter.py
+++ module_converter.py
@@ -2,34 +2,53 @@ import module_write_text as wr_module
 import pandas as pd
 
 def xlsx_to_csv(xlsx_file_path, output_path, ignore_rows=0, header_row_index=3):
+    """
+    Конвертирует все листы таблицы xlsx в csv формат и записывает в одноименные файлы
+
+    :param xlsx_file_path (str): путь к xlsx файлу
+    :param output_path (str): путь к папке для записи результа
+    :param ignore_rows (int): количество строк, которые надо проигнорировать
+    :param header_row_index (int): номер строки, которую нужно использовать как заголовок csv-файла
+    :return: None
+    """
     excel_file = pd.ExcelFile(xlsx_file_path)
 
     for sheet_name in excel_file.sheet_names:
-        csv_content = xlsx_sheet_to_csv(excel_file, sheet_name, ignore_rows, output_path)
+        csv_content = xlsx_sheet_to_csv(excel_file, sheet_name, ignore_rows)
 
         csv_header = xlsx_row_to_csv_row(excel_file, sheet_name, header_row_index)
 
         content = f"{csv_header}\n{csv_content}"
-        # content=f"{csv_content}"
         content = '\n'.join(line for line in content.splitlines() if line.strip())
 
-        output_file_path = f"{output_path}\\{sheet_name}.csv"
+        output_file_path = f"{output_path}\\{sheet_name}.csv" # составляем путь к файлу по имени листа
 
-        wr_module.str_to_file(output_file_path, content)
-        # add_header(csv_file_path, csv_row)
-        # print(f"Сохранен: {csv_file_name}")
+        wr_module.str_to_file(output_file_path, content)  # используем простую запись текста в файл
 
 
-def xlsx_sheet_to_csv(excel_file, sheet_name, ignore_rows, output_path):
+def xlsx_sheet_to_csv(excel_file, sheet_name, ignore_rows):
+    """
+    Преобразует содержимое листа xlsx таблицы в csv формат
+
+        :param excel_file (class ExcelFile): объект таблицы
+        :param sheet_name (str): Название листа
+        :param ignore_rows (int): Сколько строк таблиц проигнорировать
+        :return (str): Содержимое листа в формате csv
+    """
     df = pd.read_excel(excel_file, sheet_name=sheet_name, skiprows=ignore_rows)
-
-    csv_file_path = f"{output_path}\\{sheet_name}.csv"
-    # df.to_csv( index=False, encoding='utf-8', sep=";")
 
     return df.to_csv(index=False, encoding='utf-8', sep=";")
 
 
 def xlsx_row_to_csv_row(excel_file, sheet_name, row_index):
+    """
+    Преобразует строку с листа xlsx таблицы в csv формат
+
+        :param excel_file (class ExcelFile): объект таблицы
+        :param sheet_name (str): Название листа
+        :param row_index (int): Номер строки, которую нужно преобразовать
+        :return (str): Содержимое строки листа в формате csv
+    """
     df = pd.read_excel(excel_file, sheet_name=sheet_name)
     row = df.iloc[row_index]
     csv_row = ';'.join(map(str, row))
@@ -37,16 +56,7 @@ def xlsx_row_to_csv_row(excel_file, sheet_name, row_index):
     return csv_row
 
 
-def add_header(file_path, header):
-    content = []
-    with open(file_path, 'r') as file:
-        content = file.readlines()
-    # Добавляем новую строку в начало
-    content.insert(0, header + '\n')
 
-    # Записываем обратно в файл
-    with open(file_path, 'w') as file:
-        file.writelines(content)
 
 
 def test_xlsx_to_csv():
--- module_read_text.py
+++ module_read_text.py
@@ -4,12 +4,17 @@ import json
 
 
 # Считывание ID хостгрупп
-def read_hostgroup_ids(path):
+def read_hostgroup_ids(filepath):
+    """
+    Считывает id хост-групп из файла
+
+        :param filepath (str): путь к файлу
+        :return (dict): Словарь, ключ - имя хост группы, значение - id
+    """
     hostgroups = {}
-    with open(path, mode='r', newline='') as csv_file:
+    with open(filepath, mode='r', newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
-        # Пропускаем заголовок
-        next(csv_reader)
+        next(csv_reader)  # Пропускаем заголовок
         for row in csv_reader:
             hostgroup_id = row[0]
             hostgroup_name = row[1]
@@ -18,25 +23,31 @@ def read_hostgroup_ids(path):
     return hostgroups
 
 
-def read_id_hostgroups(path):
-    hostgroups = {}
-    with open(path, mode='r', newline='') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=';')
-        # Пропускаем заголовок
-        next(csv_reader)
-        for row in csv_reader:
-            hostgroup_id = row[0]
-            hostgroup_name = row[1]
-            hostgroups[int(hostgroup_id)] = hostgroup_name
+# def read_id_hostgroups(path):
+#
+#     hostgroups = {}
+#     with open(path, mode='r', newline='') as csv_file:
+#         csv_reader = csv.reader(csv_file, delimiter=';')
+#         # Пропускаем заголовок
+#         next(csv_reader)
+#         for row in csv_reader:
+#             hostgroup_id = row[0]
+#             hostgroup_name = row[1]
+#             hostgroups[int(hostgroup_id)] = hostgroup_name
+#
+#     return hostgroups
 
-    return hostgroups
 
 
-# Считывание пар хост-групп в пределах сервиса
 
-def read_hostgroup_pairs(path, file_list):
-    # складируем списки пар для каждого сервиса в словарь
-    # название сервиса - ключ к списку пар
+def read_hostgroup_pairs(path, file_list): # Считывание пар хост-групп в пределах сервиса
+    """
+    Определяет список пар хост-групп для каждого сервиса
+
+        :param path (str): Путь к папке с файлами, содержащими пары хостов для каждого сервиса
+        :param file_list (list(str)): Список имен файлов
+        :return (dict): Словарь, где ключ - имя сервиса (str), значение - список пар хост-групп [list(str)]
+    """
     services = {}
     for filename in file_list:
         name = filename.replace('.csv', '')
@@ -46,22 +57,41 @@ def read_hostgroup_pairs(path, file_list):
             next(csv_reader)
             for row in csv_reader:
                 hostgroup_pair = row[0]
-                pair_list.append(hostgroup_pair)
-        services[name] = pair_list
+                pair_list.append(hostgroup_pair) # складируем списки пар для каждого сервиса в словарь
+        services[name] = pair_list # название сервиса - ключ к списку пар
 
     return services
 
 
-# проверяет директорию, возвращает список имен csv файлов
-def filter_csv_files(path):
+
+def filter_csv_files(path): # проверяет директорию, возвращает список имен csv файлов
+    """
+    Возвращает список имен csv-файлов в папке
+
+        :param path (str): путь к папке
+        :return (list(str)): Список имен csv-файлов
+    """
     return filter_files(path, "csv")
 
 
 def filter_txt_files(path):
+    """
+    Возвращает список имен txt-файлов в папке
+
+        :param path (str): путь к папке
+        :return (list(str)): Список имен txt-файлов
+    """
     return filter_files(path, "txt")
 
 
 def filter_files(path, extension):
+    """
+    Возвращает список имен файлов с расширением extension
+
+        :param path (str): Путь к папке с файлами
+        :param extension (str): Расширение файла
+        :return (list(str)): Список имен extension-файлов
+    """
     files = os.listdir(path)
     filtered = []
     for i in files:
@@ -72,13 +102,24 @@ def filter_files(path, extension):
 
 
 def read_services(path):
+    """
+    Определяет список пар хост-групп для каждого сервиса, описанного в .csv файлах в папке path
+
+        :param path (str): Путь к папке
+        :return (dict): Словарь, где ключ - имя сервиса (str), значение - список пар хост-групп [list(str)]
+    """
     file_list = filter_csv_files(path)
     services = read_hostgroup_pairs(path, file_list)
-    # print(services)
     return services
 
 
 def read_config(file_path):
+    """
+    Считывает конфигурацию из файла
+
+        :param file_path (str): Файл конфигурации
+        :return (dict): Конфигурация
+    """
     f = open(file_path, 'r', encoding="utf-8")  # дополнительно обозначил кодировку, т.к. json выдает cp-1251
     config = json.load(f)
     return config
